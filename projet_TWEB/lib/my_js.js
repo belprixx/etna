f8b1da7695d43b99147818847936443ae1fd85cb
Theo Tison <tison.theo@gmail.com>
2016-04-27 00:43:42 +0200

    modal connextion
    blop

--- projet_TWEB/lib/my_js.js
+++ projet_TWEB/lib/my_js.js
@@ -1,3 +1,10 @@
 /**
  * Created by theo on 26/04/16.
  */
+$(document).ready(function(){
+    $('#co').click(function () {
+    });
+    $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').focus()
+    })
+});
