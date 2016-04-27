a0fb51d3b8dc0c2383671c5e8e42a0c8820463f9
Theo Tison <tison.theo@gmail.com>
2016-04-27 18:42:18 +0200

    modif index js et css + add image

--- projet_TWEB/lib/my_js.js
+++ projet_TWEB/lib/my_js.js
@@ -2,9 +2,7 @@
  * Created by theo on 26/04/16.
  */
 $(document).ready(function(){
-    $('#co').click(function () {
-    });
     $('#myModal').on('shown.bs.modal', function () {
         $('#myInput').focus()
-    })
+    });
 });
