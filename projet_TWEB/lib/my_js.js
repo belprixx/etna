78267c9cd71e09673ba8d8617ac61f545ca456e6
Theo Tison <tison.theo@gmail.com>
2016-04-28 22:22:02 +0200

    add user + add flag + resize
    blop

--- projet_TWEB/lib/my_js.js
+++ projet_TWEB/lib/my_js.js
@@ -5,4 +5,29 @@ $(document).ready(function(){
     $('#myModal').on('shown.bs.modal', function () {
         $('#myInput').focus()
     });
+    $('#contact_form').click(function () {
+        console.log('cc');
+
+        if ($('#prenom').val() != '' && $('#nom').val() != '' && $('#mail').val() != '')
+        {
+            console.log('toto');
+            $('#alert_contact_success').show()
+        }
+        else
+        {
+            $('#alert_contact_fail').show();
+        }
+    });
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
 });
